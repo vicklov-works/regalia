b5784cc723f7d541f956d388d3b557975967d6b0
Ifeoluwa Alabi <alabiifeoluwa0002@gmail.com>
2026-01-16 19:27:48 +0100

    Improved mobile responsiveness

--- js/script.js
+++ js/script.js
@@ -28,3 +28,19 @@ toggleButton.addEventListener('click', () => {
         localStorage.setItem('theme', 'dark');
     }
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+if (menuToggle && navLinks) {
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+}
+
+navLinks.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
+
